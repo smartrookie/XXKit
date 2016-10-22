1edca3e20b172d0924043d3e236c83a92444e44d
rookie <1223352800@qq.com>
2016-10-22 15:05:34 +0800

    Initial Commit

---
+++ XXKit/XXKit.h
@@ -0,0 +1,19 @@
+//
+//  XXKit.h
+//  XXKit
+//
+//  Created by rookie on 16/10/22.
+//  Copyright © 2016年 rookie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XXKit.
+FOUNDATION_EXPORT double XXKitVersionNumber;
+
+//! Project version string for XXKit.
+FOUNDATION_EXPORT const unsigned char XXKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XXKit/PublicHeader.h>
+
+
